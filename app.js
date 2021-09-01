6c62d19e6aee049cf517c32bceaf1d2b0411b562
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-09-01 11:43:33 +0100

    add conection configuration to mysql

---
+++ app.js
@@ -0,0 +1,38 @@
+const inquirer = require("inquirer");
+ require("./lib");
+ const mysql = require('mysql2');
+
+const log=console.log;
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'qazQAZ2021!',
+    database: 'employees_db',
+  },
+  console.log(`Database is connected.`)
+);
+
+
+const promptUser = question => {
+    return inquirer.prompt(question);
+  };
+
+
+const chooseOption = () => {
+    console.clear();
+    promptUser(options)
+    .then((userOption) => {        
+        handleOptionResponse(userOption.toDo);
+        
+        })
+    .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+            console.error(error)
+        }
+
+})
+}
+chooseOption();
